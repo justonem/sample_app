8ab952dcd2a3be9354c1074249d2741048782fd0
justonem <yesjustonem@gmail.com>
2011-03-21 13:07:46 -0400

    user model with passwords, show user page

--- Gemfile
+++ Gemfile
@@ -2,17 +2,24 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.5'
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
-gem 'awesome_print', :require => 'ap'
+gem 'gravatar_image_tag', '1.0.0'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
   gem 'annotate-models', '1.0.4'
+  gem 'awesome_print', :require => 'ap'
+  gem 'wirble'
+  gem 'utility_belt'
 end
 
 group :test do
   gem 'rspec', '2.5.0'
   gem 'webrat', '0.7.1'
   gem 'spork', '0.9.0.rc4'
+  gem 'awesome_print', :require => 'ap'
+  gem 'wirble'
+  gem 'utility_belt'
+  gem 'factory_girl_rails', '1.0'
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,63 @@
 # == Schema Information
-# Schema version: 20110311205618
+# Schema version: 20100822204528
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor   :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
-  email_regex = /\A[a-zA-Z0-9_.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\z/i
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
-  validates :name,  :presence => true,
-                    :length => { :maximum => 50 }
-  validates :email, :presence => true,
-                    :format => { :with => email_regex},
-                    :uniqueness => { :case_sensitive => false }
-                  
-end
+  validates :name,     :presence => true,
+                       :length   => { :maximum => 50 }
+  validates :email,    :presence   => true,
+                       :format     => { :with => email_regex },
+                       :uniqueness => { :case_sensitive => false }
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+  
+    # Return true if the user's password matches teh submitted password
+    def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+    end
+  
+    def self.authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+    
+    private
+      
+      def encrypt_password
+        self.salt = make_salt if new_record?
+        self.encrypted_password = encrypt(password)
+      end
+
+      def encrypt(string)
+        secure_hash("#{salt}--#{string}")
+      end
+
+      def make_salt
+        secure_hash("#{Time.now.utc}--#{password}")
+      end
+
+      def secure_hash(string)
+        Digest::SHA2.hexdigest(string)
+      end
+
+end  # class User
